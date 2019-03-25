45e82b9ca83ea492adfbd8ff1aefa855351c9887
ahwayakchih <ahwayakchih@gmail.com>
2019-03-25 19:44:19 +0100

    test: rephrase one of descriptions

--- test/lib/findCommonPath.js
+++ test/lib/findCommonPath.js
@@ -7,7 +7,7 @@ test('findCommonPath', t => {
 	t.strictEqual(findCommonPath(), '', 'Should return empty string when called without arguments');
 	t.strictEqual(findCommonPath([]), '', 'Should return empty string when files array is empty');
 	t.strictEqual(findCommonPath(['example.txt']), '', 'Should return empty string when no file has separator in path');
-	t.strictEqual(findCommonPath(['a/']), 'a/', 'Should return directory when the files contain onl directory');
+	t.strictEqual(findCommonPath(['a/']), 'a/', 'Should return directory when the files contain just a directory');
 	t.strictEqual(findCommonPath(['a/example.txt']), 'a/', 'Should return directory when the files contain one path');
 	t.strictEqual(findCommonPath(['a/example.txt', 'a/b/example.txt']), 'a/', '[a/f, a/b/f] should return "a/"');
 	t.strictEqual(findCommonPath(['a/b/example.txt', 'a/example.txt']), 'a/', '[a/b/f, a/f] should return "a/"');
