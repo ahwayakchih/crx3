b34d3f37ba09dbd30841a000b32e2ac24457d0d6
ahwayakchih <ahwayakchih@gmail.com>
2019-03-26 21:06:17 +0100

    test: improve `getFilePaths` test, to check if recursing into subdirectories works correctly

--- test/lib/getFilePaths.js
+++ test/lib/getFilePaths.js
@@ -11,11 +11,12 @@ test('getFilePaths', t => {
 	temp = getFilePaths('test');
 	t.ok(Array.isArray(temp) && temp.length === 0, 'Should return empty array when called with string instead of array');
 
-	temp = getFilePaths([__dirname]);
+	const parentDirname = path.dirname(__dirname);
+	temp = getFilePaths([parentDirname]);
 	t.ok(Array.isArray(temp) && temp.length > 0, 'Should return non-empty array when called with array');
 	t.ok(temp.indexOf(__filename) >= 0, 'Returned array should contain path to this test file');
 
-	var temp2 = getFilePaths([path.join(__dirname, 'manifest.json')]);
+	var temp2 = getFilePaths([path.join(parentDirname, 'manifest.json')]);
 	t.ok(Array.isArray(temp2) && temp2.length > 0, 'Should return non-empty array when called with a single path to "manifest.json"');
 	t.ok(temp2.indexOf(__filename) >= 0, 'Returned array should contain path to this test file');
 	t.strictEqual(temp.join(','), temp2.join(','), 'Should return the same list');
