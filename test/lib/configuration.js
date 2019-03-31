e639cfed4b0d5c190edb1466d7470be5561c6962
ahwayakchih <ahwayakchih@gmail.com>
2019-03-31 11:31:43 +0200

    test: each check should use new Configuration object

--- test/lib/configuration.js
+++ test/lib/configuration.js
@@ -7,7 +7,7 @@ test('configuration', t => {
 	const CWD = process.cwd();
 	const dirName = 'example-extension';
 
-	const c = config();
+	var c = config();
 	t.strictEqual(typeof c, 'object', 'Should create an object');
 	t.strictEqual(typeof c.helpText, 'function', 'Created object should have `helpText` method');
 	t.strictEqual(typeof c.setFromOptions, 'function', 'Created object should have `setFromOptions` method');
@@ -22,7 +22,7 @@ test('configuration', t => {
 	t.ok(!c.zipPath, 'Should not set ZIP file path by default');
 	t.ok(!c.xmlPath, 'Should not set XML file path by default');
 
-	c.setFromOptions({
+	c = config().setFromOptions({
 		keyPath: true,
 		zipPath: true,
 		xmlPath: true
@@ -34,11 +34,12 @@ test('configuration', t => {
 	t.ok(c.xmlPath, 'Should use default XML file path');
 	t.ok(c.xmlPath.startsWith(CWD), 'Default XML file path should be in current work directory');
 
-	c.name = '';
-	c.crxPath = true;
-	c.keyPath = true;
-	c.zipPath = true;
-	c.xmlPath = true;
+	c = config().setFromOptions({
+		crxPath: true,
+		keyPath: true,
+		zipPath: true,
+		xmlPath: true
+	});
 	c.setFromOptions({srcPaths: [`${CWD}/${dirName}`]});
 
 	t.strictEqual(c.crxPath, `${CWD}/${dirName}.crx`, 'Should use target directory name as CRX file name');
@@ -46,6 +47,12 @@ test('configuration', t => {
 	t.strictEqual(c.zipPath, `${CWD}/${dirName}.zip`, 'Should use target directory name as ZIP file name');
 	t.strictEqual(c.xmlPath, `${CWD}/${dirName}.xml`, 'Should use target directory name as XML file name');
 
+	c = config().setFromOptions({
+		crxPath: true,
+		keyPath: true,
+		zipPath: true,
+		xmlPath: true
+	});
 	c.setFromOptions({srcPaths: [`${CWD}/${dirName}/manifest.json`]});
 
 	t.strictEqual(c.crxPath, `${CWD}/${dirName}.crx`, 'Should use manifest.json directory name as CRX file name');
@@ -53,16 +60,18 @@ test('configuration', t => {
 	t.strictEqual(c.zipPath, `${CWD}/${dirName}.zip`, 'Should use manifest.json directory name as ZIP file name');
 	t.strictEqual(c.xmlPath, `${CWD}/${dirName}.xml`, 'Should use manifest.json directory name as XML file name');
 
-	c.name = '';
-	c.crxPath = true;
-	c.zipPath = true;
-	c.keyPath = true;
+	c = config().setFromOptions({
+		crxPath: true,
+		keyPath: true,
+		zipPath: true,
+		xmlPath: true
+	});
 	c.setFromOptions({srcPaths: [`${CWD}/${dirName}/some-script.js`, `${CWD}/${dirName}/manifest.json`]});
 
-	t.strictEqual(c.crxPath, `${CWD}/${dirName}.crx`, 'Should find and use manifest.js directory name as CRX file name');
-	t.strictEqual(c.keyPath, `${CWD}/${dirName}.pem`, 'Should find and use manifest.js directory name as key file name');
-	t.strictEqual(c.zipPath, `${CWD}/${dirName}.zip`, 'Should find and use manifest.js directory name as ZIP file name');
-	t.strictEqual(c.xmlPath, `${CWD}/${dirName}.xml`, 'Should find and use manifest.js directory name as XML file name');
+	t.strictEqual(c.crxPath, `${CWD}/${dirName}.crx`, 'Should find and use manifest.json directory name as CRX file name');
+	t.strictEqual(c.keyPath, `${CWD}/${dirName}.pem`, 'Should find and use manifest.json directory name as key file name');
+	t.strictEqual(c.zipPath, `${CWD}/${dirName}.zip`, 'Should find and use manifest.json directory name as ZIP file name');
+	t.strictEqual(c.xmlPath, `${CWD}/${dirName}.xml`, 'Should find and use manifest.json directory name as XML file name');
 
 	t.end();
 });
