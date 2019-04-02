3b8f810fa1a67769c1170d5e39aaf70f40b4b8fd
ahwayakchih <ahwayakchih@gmail.com>
2019-04-02 11:03:04 +0200

    style: fix bin/crx3.js

--- bin/crx3.js
+++ bin/crx3.js
@@ -5,11 +5,13 @@ const config = require('../lib/configuration')().setFromArgv();
 
 /* eslint-disable no-process-exit */
 if (process.stdin.isTTY && (!config.srcPaths || config.srcPaths.length < 1)) {
+	/* eslint-disable prefer-named-capture-group */
 	console.log(config
 		.helpText()
 		.replace(/```sh\n+([\w\W]+?)\n+```/g, (_, sh) => `\u001b[1m${sh}\u001b[0m`)
 		.replace(/\*\*([^*]+?)\*\*/g, (_, bold) => `\u001b[7;1m${bold}\u001b[0m`)
 		.replace(/\*([^*]+?)\*/g, (_, italic) => `\u001b[1m${italic}\u001b[0m`));
+	/* eslint-enable prefer-named-capture-group */
 	process.exit();
 }
 
