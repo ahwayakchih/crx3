13245934becf99af00e5ea94a16d70f33ba46826
ahwayakchih <ahwayakchih@gmail.com>
2019-10-08 18:33:07 +0200

    test: check if null ending is required when calling `reg add`

--- test/lib/writeCRX3File.js
+++ test/lib/writeCRX3File.js
@@ -254,7 +254,7 @@ async function doesItWorkInChrome (t, cfg) {
 
 	const msgPolicy = 'Should add policy required for test';
 	if (OS === 'win32') {
-		tryExec(t, 'reg add HKLM\\Software\\Policies\\Google\\Chrome\\ExtensionSettings /v crx3-example-extension-test /t REG_SZ /d ' + testPolicy + '\\0 /f', msgPolicy);
+		tryExec(t, 'reg add HKLM\\Software\\Policies\\Google\\Chrome\\ExtensionSettings /v crx3-example-extension-test /t REG_SZ /d ' + testPolicy + ' /f', msgPolicy);
 		tryExec(t, 'reg query HKLM\\Software\\Policies\\Google\\Chrome\\ExtensionSettings /v crx3-example-extension-test', 'Check if registry was changed');
 	}
 	else {
