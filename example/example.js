e4503b4cf50ef9a977751136d8837b62ce613029
ahwayakchih <ahwayakchih@gmail.com>
2019-10-08 20:38:53 +0200

    fix: make example generate exactly the same manifest file

--- example/example.js
+++ example/example.js
@@ -6,7 +6,8 @@ crx3(['example/example-extension/manifest.json'], {
 	keyPath: 'example/example-extension.pem',
 	crxPath: 'example/example-extension.crx',
 	zipPath: 'example/example-extension.zip',
-	xmlPath: 'example/example-extension.xml'
+	xmlPath: 'example/example-extension.xml',
+	crxURL : 'http://127.0.0.1:8080/example-extension.crx'
 })
 	.then(() => console.log('done'))
 	.catch(console.error)
