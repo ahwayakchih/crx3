ab6a4c1cea402155c3c780f4a704230de361dc4a
ahwayakchih <ahwayakchih@gmail.com>
2020-03-29 18:26:24 +0200

    test: fix setting policy on Windows
    This still does not matter, as Chrome ignores in on Windows when not
    in active domain (or whatever it's called).

--- test/lib/support/setPolicy.js
+++ test/lib/support/setPolicy.js
@@ -11,17 +11,23 @@ SET_POLICY.linux = async function setPolicyLinux (policy) {
 };
 
 SET_POLICY.win32 = async function setPolicyWindows (policy) {
+	var error = null;
 	const fakeT = {
 		pass   : console.log,
 		comment: console.log,
-		error  : console.error
+		error  : function (err) {
+			error = err;
+			console.error(err);
+		}
 	};
 
 	// https://www.chromium.org/administrators/complex-policies-on-windows
-	tryExec(fakeT, `reg add HKLM\\Software\\Policies\\Google\\Chrome /v ExtensionSettings /t REG_SZ /d ${JSON.stringify(policy.ExtensionSettings)} /f`, 'Setting policy in Windows registry should work');
-	if (process.env.DEBUG) {
-		tryExec(fakeT, 'reg query HKLM\\Software\\Policies\\Google\\Chrome /v ExtensionSettings', 'Registry should contain our test policy');
+	var result = tryExec(fakeT, `reg add HKLM\\Software\\Policies\\Google\\Chrome /v ExtensionSettings /t REG_SZ /d ${JSON.stringify(policy.ExtensionSettings)} /f`, 'Setting policy in Windows registry should work');
+	if (result && process.env.DEBUG) {
+		result = tryExec(fakeT, 'reg query HKLM\\Software\\Policies\\Google\\Chrome /v ExtensionSettings', 'Registry should contain our test policy');
 	}
+
+	return result && !error ? Promise.resolve() : Promise.reject(error);
 };
 
 module.exports = SET_POLICY[OS] || null;
